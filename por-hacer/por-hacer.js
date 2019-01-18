0abce46e0d56ec91db3daea03d4a3fb182aa2b0e
paola <paola@appeal.com.mx>
2019-01-17 21:56:48 -0600

    primer commit

---
+++ por-hacer/por-hacer.js
@@ -0,0 +1,68 @@
+const fs=require('fs');
+let listadoPorHacer=[];
+
+const guardarDB=()=>{
+    let data=JSON.stringify(listadoPorHacer);
+    fs.writeFile('db/data.json', data, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+      });
+}
+const cargarDB=()=>{
+    try {
+        listadoPorHacer=require('../db/data.json');
+        
+    } catch (error) {
+        listadoPorHacer=[];
+    }
+    
+}
+const getListado=()=>{
+    cargarDB();
+    return listadoPorHacer;
+}
+const borrar=(descripcion)=>{
+    cargarDB();
+    let nuevalista=listadoPorHacer.filter((tarea)=>{
+        return tarea.descripcion!==descripcion;
+    });
+    
+    if(listadoPorHacer.length === nuevalista.length){
+        return('no lo borro alv');
+    }else{
+        listadoPorHacer=nuevalista;
+        guardarDB();
+        return('ya alv');
+        
+    }
+    
+}
+const crear = (descripcion) => {
+    cargarDB();
+    let porHacer={
+        descripcion,
+        completado:false
+    };
+
+    listadoPorHacer.push(porHacer);
+    guardarDB();
+    return porHacer;
+}
+const actualizar=(descripcion, completado=true)=>{
+    cargarDB();
+    let index=listadoPorHacer.findIndex(tarea=> tarea.descripcion===descripcion);
+    if(index>=0){
+        listadoPorHacer[index].completado=completado;
+        guardarDB();
+        return true;
+    }else{
+        return false;
+    }
+
+}
+module.exports={
+    crear,
+    getListado,
+    actualizar,
+    borrar
+};
